08b68052184ad74e610f51a70813e668afd1bb4c
Kamil Kasperczyk <kamil.kasperczyk@nordicsemi.no>
2021-11-16 09:48:02 +0100

    Revert "[clusters] Fix scheduled actions in clusters"
    This reverts commit 82e6977aded24f38a3eab4330d38fb9ee983e567.

--- src/test_driver/linux-cirque/MobileDeviceTest.py
+++ src/test_driver/linux-cirque/MobileDeviceTest.py
@@ -23,7 +23,7 @@ import sys
 
 from helper.CHIPTestBase import CHIPVirtualHome
 
-logger = logging.getLogger('MobileDeviceTest')
+logger = logging.getLogger('CHIPInteractionModelTest')
 logger.setLevel(logging.INFO)
 
 sh = logging.StreamHandler()

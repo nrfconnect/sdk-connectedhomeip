e386b613940b42937d6c41d84605c820e37ca801
Damian Krolik <damian.krolik@nordicsemi.no>
2022-08-01 15:23:01 +0200

    Revert "[nrf fromtree] Initialize CASESessionManager's mrpLocalConfig in Server::Init() (#19749)"
    This reverts commit 86cf62777d65b9875adbfcadcb14a009f0fa9bcf.

--- src/app/server/Server.cpp
+++ src/app/server/Server.cpp
@@ -257,7 +257,6 @@ CHIP_ERROR Server::Init(const ServerInitParams & initParams)
             .fabricTable       = &mFabrics,
             .clientPool        = &mCASEClientPool,
             .groupDataProvider = mGroupsProvider,
-            .mrpLocalConfig    = Optional<ReliableMessageProtocolConfig>(GetLocalMRPConfig()),
         },
         .devicePool        = &mDevicePool,
     };

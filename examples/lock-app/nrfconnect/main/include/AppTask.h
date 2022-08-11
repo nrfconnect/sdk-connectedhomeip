238a18db4eae15381c3c1838d8e863d841c59ead
Marcin Kajor <marcin.kajor@nordicsemi.no>
2022-08-11 15:09:12 +0200

    [nrf fromtree] Remove test mode from examples (#21676)
    It is a leftover from very early phase of the project when
    we had the redezvous bypass mode.
    
     Signed-off-by: Damian Krolik <damian.krolik@nordicsemi.no>

--- examples/lock-app/nrfconnect/main/include/AppTask.h
+++ examples/lock-app/nrfconnect/main/include/AppTask.h
@@ -59,9 +59,6 @@ private:
     static void UpdateLedStateEventHandler(AppEvent * aEvent);
     static void FunctionTimerEventHandler(AppEvent * aEvent);
     static void FunctionHandler(AppEvent * aEvent);
-#ifdef CONFIG_NET_L2_OPENTHREAD
-    static void StartThreadHandler(AppEvent * aEvent);
-#endif
     static void LockActionEventHandler(AppEvent * aEvent);
     static void StartBLEAdvertisementHandler(AppEvent * aEvent);
 

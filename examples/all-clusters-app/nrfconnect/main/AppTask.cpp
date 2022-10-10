3844daf1c038e9c6fb6d3059b6d130f85b4ecd2a
Marcin Kajor <marcin.kajor@nordicsemi.no>
2022-10-10 13:49:13 +0200

    [nrf noup] Aligned Matter SDK to Zephyr 0.15.x revision.
    These changes were implied the the recent Zephyr upmerge:
    - updates of Zephyr include paths
    - removal of disabling of gpio1 in board DTS overlays
    - fix for the possible dangling pointer compilation error
      after gcc got more picky about that (v10.3 vs. v12.1)
    
    Signed-off-by: Marcin Kajor <marcin.kajor@nordicsemi.no>

--- examples/all-clusters-app/nrfconnect/main/AppTask.cpp
+++ examples/all-clusters-app/nrfconnect/main/AppTask.cpp
@@ -42,7 +42,7 @@
 
 #include <dk_buttons_and_leds.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/zephyr.h>
+#include <zephyr/kernel.h>
 
 using namespace ::chip;
 using namespace ::chip::Credentials;

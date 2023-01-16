bccd7014eabd73ea83292fdacc0c3a85c93769b5
Damian Krolik <damian.krolik@nordicsemi.no>
2023-01-16 14:53:28 +0100

    [nrf noup] Bring back generation of callback-stub.cpp and plugin callbacks
    Allow to bypass codegen at build time.
    Re-add removed callback-stub.cpp and plugin callback templates.
    
    Signed-off-by: Damian Krolik <damian.krolik@nordicsemi.no>

--- src/app/util/util.cpp
+++ src/app/util/util.cpp
@@ -26,8 +26,11 @@
 #include <app/util/ember-compatibility-functions.h>
 #include <app/util/generic-callbacks.h>
 
-// TODO: figure out a clear path for compile-time codegen
+#ifdef CHIP_BYPASS_IDL
+#include <zap-generated/PluginApplicationCallbacks.h>
+#else
 #include <app/PluginApplicationCallbacks.h>
+#endif
 
 #ifdef EMBER_AF_PLUGIN_GROUPS_SERVER
 #include <app/clusters/groups-server/groups-server.h>

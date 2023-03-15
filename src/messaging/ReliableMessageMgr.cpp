68d6eddc4f77e47c7eca3cfde14f6b08776aa91e
Kamil Kasperczyk <kamil.kasperczyk@nordicsemi.no>
2023-03-15 13:02:38 +0100

    Revert "[nrf fromlist] [mrp] Increase MRP intervals for Thread"
    This reverts commit f8cafb3d632bce9de8760e0fff81d8bc9ed66fb8.

--- src/messaging/ReliableMessageMgr.cpp
+++ src/messaging/ReliableMessageMgr.cpp
@@ -263,10 +263,6 @@ System::Clock::Timestamp ReliableMessageMgr::GetBackoff(System::Clock::Timestamp
     }
 #endif
 
-#ifdef CHIP_CONFIG_MRP_RETRY_INTERVAL_SENDER_BOOST
-    mrpBackoffTime += CHIP_CONFIG_MRP_RETRY_INTERVAL_SENDER_BOOST;
-#endif
-
     return mrpBackoffTime;
 }
 

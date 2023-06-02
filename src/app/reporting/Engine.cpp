3607acc89c6a1287c636fbf6c23c8f0976dd4b5d
Hrishikesh Dhayagude <dhrishi@users.noreply.github.com>
2023-06-03 03:49:41 +0530

    [nrf fromtree] Add a lint for SuccessOrExit without assignment. (#26854) (#27029)
    * Add a lint for SuccessOrExit without assignment.
    
    This is almost always a mistake, since it loses track of the error involved.
    
    * Address review comment.
    
    Co-authored-by: Boris Zbarsky <bzbarsky@apple.com>
    
    (cherry picked from commit 0f8fac1f86a91fd3fe4e1056d546ef00de72e46f)

--- src/app/reporting/Engine.cpp
+++ src/app/reporting/Engine.cpp
@@ -530,7 +530,7 @@ CHIP_ERROR Engine::BuildAndSendSingleReportData(ReadHandler * apReadHandler)
         }
     }
 
-    SuccessOrExit(reportDataBuilder.GetError());
+    SuccessOrExit(err = reportDataBuilder.GetError());
     SuccessOrExit(err = reportDataWriter.UnreserveBuffer(kReservedSizeForMoreChunksFlag + kReservedSizeForIMRevision +
                                                          kReservedSizeForEndOfReportMessage));
     if (hasMoreChunks)

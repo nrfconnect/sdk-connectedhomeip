ea0f43a3414623e8f7510fd8c33a1e1840a444ba
Michał Szablowski <michal.szablowski@nordicsemi.no>
2023-07-06 15:07:27 +0200

    [nrf noup] Fix generate zap sript on windows
    Usage of generate.py script is not possible on windows
    due to fcntl package available onlu on unix pyhton packages
    using I/O control on descriptor file is not needed on windows.
    This change enhance script to use fcntl on non-window system.

--- scripts/tools/zap/generate.py
+++ scripts/tools/zap/generate.py
@@ -16,7 +16,7 @@
 #
 
 import argparse
-import fcntl
+import platform
 import json
 import os
 import shutil
@@ -29,6 +29,13 @@ from pathlib import Path
 from typing import Optional
 
 from zap_execution import ZapTool
+
+def isWindows():
+    return platform.system() == "Windows"
+
+# fcntl is not supported on widows platform due to lack of necessity of I/O control on file descriptor
+if not isWindows():
+    import fcntl
 
 
 @dataclass
@@ -330,16 +337,17 @@ class LockFileSerializer:
         self.lock_file = None
 
     def __enter__(self):
-        if not self.lock_file_path:
+        # fcntl is not supported on widows platform due to lack of necessity of I/O control on file descriptor
+        if not self.lock_file_path or isWindows():
             return
-
         self.lock_file = open(self.lock_file_path, 'wb')
         fcntl.lockf(self.lock_file, fcntl.LOCK_EX)
 
-    def __exit__(self, *args):
-        if not self.lock_file:
-            return
 
+    def __exit__(self, *args):
+        # fcntl is not supported on widows platform due to lack of necessity of I/O control on file descriptor
+        if not self.lock_file or isWindows():
+            return
         fcntl.lockf(self.lock_file, fcntl.LOCK_UN)
         self.lock_file.close()
         self.lock_file = None

5d759e7487792b892995bd85c072f9f8d005d649
Boris Zbarsky <bzbarsky@apple.com>
2023-08-08 11:33:21 -0400

    [nrf fromtree] Improve size calculation for our packets. (#28563)
    * Improve size calculation for our packets.
    
    We were hardcoding kMaxAppMessageLen and kMaxSecureSduLengthBytes, but those
    defines were not even consistent with each other, and both were overly
    conservative.  Specific changes:
    
    1. Fix computation of CHIP_SYSTEM_HEADER_RESERVE_SIZE: we don't have any "crypto
       headers".
    2. Fix computation of kMaxAppMessageLen to use our actual packet buffer sizes
       and defined header sizes.
    3. Fix kMaxSecureSduLengthBytes to be consistent with kMaxAppMessageLen.
    
    * Simplify the size computation logic.

--- src/app/tests/TestWriteInteraction.cpp
+++ src/app/tests/TestWriteInteraction.cpp
@@ -580,8 +580,9 @@ void TestWriteInteraction::TestWriteHandlerReceiveInvalidMessage(nlTestSuite * a
     err           = engine->Init(&ctx.GetExchangeManager(), &ctx.GetFabricTable());
     NL_TEST_ASSERT(apSuite, err == CHIP_NO_ERROR);
 
+    // Reserve all except the last 128 bytes, so that we make sure to chunk.
     app::WriteClient writeClient(&ctx.GetExchangeManager(), &writeCallback, Optional<uint16_t>::Missing(),
-                                 static_cast<uint16_t>(900) /* reserved buffer size */);
+                                 static_cast<uint16_t>(kMaxSecureSduLengthBytes - 128) /* reserved buffer size */);
 
     ByteSpan list[5];
 
@@ -648,8 +649,9 @@ void TestWriteInteraction::TestWriteHandlerInvalidateFabric(nlTestSuite * apSuit
     err           = engine->Init(&ctx.GetExchangeManager(), &ctx.GetFabricTable());
     NL_TEST_ASSERT(apSuite, err == CHIP_NO_ERROR);
 
+    // Reserve all except the last 128 bytes, so that we make sure to chunk.
     app::WriteClient writeClient(&ctx.GetExchangeManager(), &writeCallback, Optional<uint16_t>::Missing(),
-                                 static_cast<uint16_t>(900) /* reserved buffer size */);
+                                 static_cast<uint16_t>(kMaxSecureSduLengthBytes - 128) /* reserved buffer size */);
 
     ByteSpan list[5];
 

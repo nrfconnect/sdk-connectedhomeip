1dffeb48f087bd3363edaeda03e5d105cb6fa70b
Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>
2023-10-13 09:32:56 +0200

    [nrf fromlist] [nrfconnect] Add a delay to last fabric remove action
    In some cases, a last fabric removal action should be delayed
    to avoid race conditions and allow the device to finish all
    currently running tasks.
    
    To control the delay use the
    CONFIG_CHIP_LAST_FABRIC_REMOVED_SCHEDULE_TIME kconfig.
    By default it has been set to 500 ms.

--- examples/light-switch-app/nrfconnect/main/include/AppTask.h
+++ examples/light-switch-app/nrfconnect/main/include/AppTask.h
@@ -41,13 +41,10 @@
 
 struct k_timer;
 struct Identify;
-class AppFabricTableDelegate;
 
 class AppTask
 {
 public:
-    friend class AppFabricTableDelegate;
-
     static AppTask & Instance()
     {
         static AppTask sAppTask;

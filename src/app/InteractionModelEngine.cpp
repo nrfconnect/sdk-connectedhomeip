c849f20b80f4aeb48e8be2b74e0388276d1c2855
Kamil Kasperczyk <kamil.kasperczyk@nordicsemi.no>
2023-11-09 08:34:39 +0100

    Revert "[nrf fromtree] Fix cancellation of subscriptions to work correctly. (#28569)"
    This reverts commit 5e90d7dbe5583c939d4e314cd29ea023a474e9c0.

--- src/app/InteractionModelEngine.cpp
+++ src/app/InteractionModelEngine.cpp
@@ -453,14 +453,14 @@ Protocols::InteractionModel::Status InteractionModelEngine::OnReadInitialRequest
             // Walk through all existing subscriptions and shut down those whose subscriber matches
             // that which just came in.
             //
-            mReadHandlers.ForEachActiveObject([apExchangeContext](ReadHandler * handler) {
+            mReadHandlers.ForEachActiveObject([this, apExchangeContext](ReadHandler * handler) {
                 if (handler->IsFromSubscriber(*apExchangeContext))
                 {
                     ChipLogProgress(InteractionModel,
                                     "Deleting previous subscription from NodeId: " ChipLogFormatX64 ", FabricIndex: %u",
                                     ChipLogValueX64(apExchangeContext->GetSessionHandle()->AsSecureSession()->GetPeerNodeId()),
                                     apExchangeContext->GetSessionHandle()->GetFabricIndex());
-                    handler->Close();
+                    mReadHandlers.ReleaseObject(handler);
                 }
 
                 return Loop::Continue;

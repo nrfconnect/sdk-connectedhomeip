a73987f7e0761873a5222e1678366ba497738b7c
Damian Krolik <damian.krolik@nordicsemi.no>
2023-11-16 16:15:23 +0100

    [nrf fromlist] [dns-sd] Untangle dependencies between Resolver and ResolverProxy
    The relationship between Resolver and Resolver Proxy is
    unclear as both classes depend on each other and Resolver
    Proxy is implemented in a source file of concrete Resolver
    implementation. This makes it impossible to build Matter
    library with several Resolver implementations and select
    one to be used at runtime.
    
    The main reason for introducing Resolver Proxy was the need
    for running multiple parallel DNS-SD queries. Address this
    by adding Discovery Delegate parameter to the Resolver
    interface methods for starting and stopping the discovery.
    
    Then implement Resolver Proxy as a convenience class on top
    of the Resolver interface. Additionally, simplify the
    Commissionable Node Controller class by injecting a test
    Resolver into Resolver Proxy.

--- src/lib/shell/commands/Dns.cpp
+++ src/lib/shell/commands/Dns.cpp
@@ -198,12 +198,7 @@ bool ParseSubType(int argc, char ** argv, Dnssd::DiscoveryFilter & filter)
 CHIP_ERROR BrowseCommissionableHandler(int argc, char ** argv)
 {
     Dnssd::DiscoveryFilter filter;
-
-    if (!ParseSubType(argc, argv, filter))
-    {
-        streamer_printf(streamer_get(), "Invalid argument\r\n");
-        return CHIP_ERROR_INVALID_ARGUMENT;
-    }
+    VerifyOrReturnError(ParseSubType(argc, argv, filter), CHIP_ERROR_INVALID_ARGUMENT);
 
     streamer_printf(streamer_get(), "Browsing commissionable nodes...\r\n");
 
@@ -213,12 +208,7 @@ CHIP_ERROR BrowseCommissionableHandler(int argc, char ** argv)
 CHIP_ERROR BrowseCommissionerHandler(int argc, char ** argv)
 {
     Dnssd::DiscoveryFilter filter;
-
-    if (!ParseSubType(argc, argv, filter))
-    {
-        streamer_printf(streamer_get(), "Invalid argument\r\n");
-        return CHIP_ERROR_INVALID_ARGUMENT;
-    }
+    VerifyOrReturnError(ParseSubType(argc, argv, filter), CHIP_ERROR_INVALID_ARGUMENT);
 
     streamer_printf(streamer_get(), "Browsing commissioners...\r\n");
 
@@ -233,6 +223,9 @@ CHIP_ERROR BrowseHandler(int argc, char ** argv)
         return CHIP_NO_ERROR;
     }
 
+    sResolverProxy.Init(DeviceLayer::UDPEndPointManager());
+    sResolverProxy.SetCommissioningDelegate(&sDnsShellResolverDelegate);
+
     return sShellDnsBrowseSubcommands.ExecCommand(argc, argv);
 }
 
@@ -243,9 +236,6 @@ CHIP_ERROR DnsHandler(int argc, char ** argv)
         sShellDnsSubcommands.ForEachCommand(PrintCommandHelp, nullptr);
         return CHIP_NO_ERROR;
     }
-
-    sResolverProxy.Init(DeviceLayer::UDPEndPointManager());
-    sResolverProxy.SetCommissioningDelegate(&sDnsShellResolverDelegate);
 
     return sShellDnsSubcommands.ExecCommand(argc, argv);
 }

4936f9fa2be9e6cd16b02e4fcb9a7aff611f4981
Patryk Lipinski <patryk.lipinski@nordicsemi.no>
2024-01-19 15:37:43 +0100

    [nrf noup] Lock thread stack before factory reset
    This commit fix a problem with thread activity interrupting factory reset.
    This activity used to led to “Factory reset fail: -6”.
    Writing to cleared nvm flash pages caused the problem.
    
    Signed-off-by: Patryk Lipinski <patryk.lipinski@nordicsemi.no>
    Signed-off-by: Michał Szablowski <michal.szablowski@nordicsemi.no>

--- src/platform/Zephyr/ConfigurationManagerImpl.cpp
+++ src/platform/Zephyr/ConfigurationManagerImpl.cpp
@@ -40,6 +40,10 @@
 #include <zephyr/settings/settings.h>
 #endif
 
+#ifdef CONFIG_NET_L2_OPENTHREAD
+#include <platform/ThreadStackManager.h>
+#endif 
+
 namespace chip {
 namespace DeviceLayer {
 
@@ -176,6 +180,11 @@ void ConfigurationManagerImpl::RunConfigUnitTest(void)
 void ConfigurationManagerImpl::DoFactoryReset(intptr_t arg)
 {
     ChipLogProgress(DeviceLayer, "Performing factory reset");
+
+// Lock the Thread stack to avoid unwanted interaction with settings NVS during factory reset.
+#ifdef CONFIG_NET_L2_OPENTHREAD
+    ThreadStackMgr().LockThreadStack();
+#endif
 
 #ifdef CONFIG_CHIP_FACTORY_RESET_ERASE_NVS
     void * storage = nullptr;

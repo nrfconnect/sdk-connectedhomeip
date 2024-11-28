810e8f3134c4d72bc47f2ce571ca857a9d0128c0
Adrian Gielniewski <adrian.gielniewski@nordicsemi.no>
2024-11-28 09:18:41 +0100

    Revert "[nrf toup] Fix defines for ZMS"
    This reverts commit 5148b72bda4773ccaaf7df52149f765f01248079.
    
    Signed-off-by: Adrian Gielniewski <adrian.gielniewski@nordicsemi.no>

--- src/platform/Zephyr/ConfigurationManagerImpl.cpp
+++ src/platform/Zephyr/ConfigurationManagerImpl.cpp
@@ -39,9 +39,9 @@
 
 #ifdef CONFIG_CHIP_FACTORY_RESET_ERASE_SETTINGS
 #include <zephyr/settings/settings.h>
-#if defined(CONFIG_SETTINGS_NVS)
+#ifdef CONFIG_SETTINGS_NVS
 #include <zephyr/fs/nvs.h>
-#elif defined(CONFIG_SETTINGS_ZMS)
+#elif CONFIG_SETTINGS_ZMS
 #include <zephyr/fs/zms.h>
 #endif // CONFIG_SETTINGS_NVS || CONFIG_SETTINGS_ZMS
 #endif // CONFIG_CHIP_FACTORY_RESET_ERASE_SETTINGS
@@ -204,9 +204,9 @@ void ConfigurationManagerImpl::DoFactoryReset(intptr_t arg)
 
     if (status == 0)
     {
-#if defined(CONFIG_SETTINGS_NVS)
+#ifdef CONFIG_SETTINGS_NVS
         status = nvs_clear(static_cast<nvs_fs *>(storage));
-#elif defined(CONFIG_SETTINGS_ZMS)
+#elif CONFIG_SETTINGS_ZMS
         status = zms_clear(static_cast<zms_fs *>(storage));
 #endif // CONFIG_SETTINGS_NVS || CONFIG_SETTINGS_ZMS
     }

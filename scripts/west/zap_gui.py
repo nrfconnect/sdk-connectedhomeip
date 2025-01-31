e8f1332e0861007a59c6c987e0ca5355023234ce
Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>
2025-01-31 10:37:08 +0100

    [nrf noup] Added zap_append to west command
    Added zap_append command to the west.
    
    use `west zap-append -h` to see all options.
    
    Now you can add new clusters to the matter ZCL datamodel.
    Use a `west zap-append` and provide the new cluster
    definitionsas XML files to add them to the zcl.json file.
    You can generate the new zcl.json file by providing
    `-o, --output` argument, or overwrite the existing one.
    
    The zcl_generate is integrated with `west zap-gui` commands.
    If you want to use it call the `west zap-gui` command with
    the additional argument `--clusters`.
    You can provide multiple clusters at once.
    If you provide an additional `-j` / `--zcl-json` argument
    alongside the `--clusters` argument to the `west zap-gui`
    command, the new zcl.json will be created and contain
    the new clusters.
    
    Signed-off-by: Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>

--- scripts/west/zap_gui.py
+++ scripts/west/zap_gui.py
@@ -3,12 +3,15 @@
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 
 import argparse
+from pathlib import Path
 
 from textwrap import dedent
 
 from west.commands import WestCommand
+from west import log
 
-from zap_common import existing_file_path, existing_dir_path, find_zap, ZapInstaller, DEFAULT_MATTER_PATH
+from zap_common import existing_file_path, existing_dir_path, find_zap, ZapInstaller, DEFAULT_MATTER_PATH, DEFAULT_ZCL_JSON_RELATIVE_PATH, DEFAULT_APP_TEMPLATES_RELATIVE_PATH, update_zcl_in_zap
+from zap_append import add_cluster_to_zcl
 
 
 class ZapGui(WestCommand):
@@ -31,33 +34,55 @@ class ZapGui(WestCommand):
                                          description=self.description)
         parser.add_argument('-z', '--zap-file', type=existing_file_path,
                             help='Path to data model configuration file (*.zap)')
-        parser.add_argument('-j', '--zcl-json', type=existing_file_path,
-                            help='Path to data model definition file (zcl.json)')
+        parser.add_argument('-j', '--zcl-json', type=str,
+                            help='Path to data model definition file (zcl.json). If new clusters are added using --clusters, the new zcl.json file will be created and used.')
         parser.add_argument('-m', '--matter-path', type=existing_dir_path,
-                            default=DEFAULT_MATTER_PATH, help='Path to Matter SDK')
+                            default=DEFAULT_MATTER_PATH, help=f'Path to Matter SDK. Default is set to {DEFAULT_MATTER_PATH}')
+        parser.add_argument('--clusters', nargs='+',
+                            help="Paths to the XML files that contain the external cluster definitions")
+        parser.add_argument('-c', '--cache', type=Path,
+                            help='Path to the custom cache directory. If not provided a temporary directory will be used and cleared after the usage.')
         return parser
 
     def do_run(self, args, unknown_args):
-        if args.zap_file:
-            zap_file_path = args.zap_file
-        else:
-            zap_file_path = find_zap()
+        default_zcl_path = args.matter_path.joinpath(DEFAULT_ZCL_JSON_RELATIVE_PATH)
 
-        if args.zcl_json:
-            zcl_json_path = args.zcl_json.absolute()
-        else:
-            zcl_json_path = args.matter_path / 'src/app/zap-templates/zcl/zcl.json'
+        zap_file_path = args.zap_file or find_zap()
+        zcl_json_path = Path(args.zcl_json).absolute() if args.zcl_json else default_zcl_path
 
-        app_templates_path = args.matter_path / 'src/app/zap-templates/app-templates.json'
+        if args.clusters:
+            # If the user provided the clusters and the zcl.json file provided by -j argument does not exist
+            # we will create a new zcl.json file according to the base zcl.json file in default_zcl_path.
+            # If the provided zcl.json file exists, we will use it as a base and update with a new cluster.
+            base_zcl = zcl_json_path if zcl_json_path.exists() else default_zcl_path
+            add_cluster_to_zcl(base_zcl, args.clusters, zcl_json_path)
+        elif not zcl_json_path.exists():
+            # If clusters are not provided, but user provided a zcl.json file we need to check whether the file exists.
+            log.err(f"ZCL file not found: {zcl_json_path}")
+            return
+
+        app_templates_path = args.matter_path.joinpath(DEFAULT_APP_TEMPLATES_RELATIVE_PATH)
+
+        log.inf(f"Using ZAP file: {zap_file_path}")
+        log.inf(f"Using ZCL file: {zcl_json_path}")
+        log.inf(f"Using app templates: {app_templates_path.absolute()}")
 
         zap_installer = ZapInstaller(args.matter_path)
         zap_installer.update_zap_if_needed()
-        zap_cache_path = zap_installer.get_install_path() / ".zap"
+
+        # The zcl.json path in the .zap file must be the same as the one provided by the user
+        # If not, update the .zap file with the new relative path to the zcl.json file.
+        # After that we must clear the ZAP cache.
+        was_updated = update_zcl_in_zap(zap_file_path, zcl_json_path, app_templates_path)
+        if args.cache and was_updated:
+            log.wrn("ZCL file path in the ZAP file has been updated. The ZAP cache must be cleared to use it.")
 
         cmd = [zap_installer.get_zap_path()]
         cmd += [zap_file_path] if zap_file_path else []
-        cmd += ["--zcl", zcl_json_path]
-        cmd += ["--gen", app_templates_path]
-        cmd += ["--stateDirectory", zap_cache_path]
-
+        cmd += ["--zcl", zcl_json_path.absolute()]
+        cmd += ["--gen", app_templates_path.absolute()]
+        if args.cache:
+            cmd += ["--stateDirectory", args.cache.absolute()]
+        else:
+            cmd += ["--tempState"]
         self.check_call([str(x) for x in cmd])

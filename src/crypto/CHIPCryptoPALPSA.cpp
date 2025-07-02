eb1b021303e3bcb9290a056a13885c1075597a64
Kamil Kasperczyk <kamil.kasperczyk@nordicsemi.no>
2025-07-02 06:52:43 +0200

    Revert "[nrf toup] [Crypto] Stop compiling legacy SPAKE2P if PSA is available"
    This reverts commit 1197041d74f1fd10bdff560d9e21d58ebad8d5ba.

--- src/crypto/CHIPCryptoPALPSA.cpp
+++ src/crypto/CHIPCryptoPALPSA.cpp
@@ -788,9 +788,6 @@ CHIP_ERROR P256Keypair::NewCertificateSigningRequest(uint8_t * out_csr, size_t &
     return CHIP_NO_ERROR;
 }
 
-// We should compile this SPAKE2P implementation only if the PSA implementation is not in use.
-#if !CHIP_CRYPTO_PSA_SPAKE2P
-
 typedef struct Spake2p_Context
 {
     mbedtls_ecp_group curve;
@@ -1095,8 +1092,6 @@ CHIP_ERROR Spake2p_P256_SHA256_HKDF_HMAC::PointIsValid(void * R)
 
     return CHIP_NO_ERROR;
 }
-
-#endif // !CHIP_CRYPTO_PSA_SPAKE2P
 
 } // namespace Crypto
 } // namespace chip

2ed8f80954d6d408446df87cc7dd5da64d84814e
Kamil Kasperczyk <kamil.kasperczyk@nordicsemi.no>
2025-07-21 10:49:48 +0200

    [nrf noup] Added measuring factory reset time
    Added functionality that can be enabled using Kconfig option
    and allows to measure the time between factory reset schedule
    and factory reset end to know this process duration.
    
    Signed-off-by: Kamil Kasperczyk <kamil.kasperczyk@nordicsemi.no>

--- src/platform/Zephyr/ConfigurationManagerImpl.cpp
+++ src/platform/Zephyr/ConfigurationManagerImpl.cpp
@@ -63,6 +63,20 @@ namespace DeviceLayer {
 
 using namespace ::chip::DeviceLayer::Internal;
 
+#ifdef CONFIG_CHIP_FACTORY_RESET_TIME_MEASUREMENT
+void FactoryResetHandler(const ChipDeviceEvent * event, intptr_t /* unused */)
+{
+    switch (event->Type)
+    {
+    case DeviceEventType::kFactoryReset:
+        ConfigurationManagerImpl::GetDefaultInstance().CaptureFactoryResetStartTime();
+        break;
+    default:
+        break;
+    }
+}
+#endif // CONFIG_CHIP_FACTORY_RESET_TIME_MEASUREMENT
+
 ConfigurationManagerImpl & ConfigurationManagerImpl::GetDefaultInstance()
 {
     static ConfigurationManagerImpl sInstance;
@@ -93,6 +107,10 @@ CHIP_ERROR ConfigurationManagerImpl::Init()
         err = StoreRebootCount(1);
         SuccessOrExit(err);
     }
+
+#ifdef CONFIG_CHIP_FACTORY_RESET_TIME_MEASUREMENT
+    PlatformMgr().AddEventHandler(FactoryResetHandler, 0);
+#endif // CONFIG_CHIP_FACTORY_RESET_TIME_MEASUREMENT
 
     err = CHIP_NO_ERROR;
 
@@ -239,6 +257,11 @@ void ConfigurationManagerImpl::DoFactoryReset(intptr_t arg)
         ChipLogError(DeviceLayer, "Factory reset failed: %" CHIP_ERROR_FORMAT, err.Format());
     }
 #endif // CONFIG_CHIP_FACTORY_RESET_ERASE_SETTINGS
+
+#ifdef CONFIG_CHIP_FACTORY_RESET_TIME_MEASUREMENT
+    ChipLogProgress(DeviceLayer, "FACTORY RESET COMPLETED, IT TOOK: %" PRIu64 " ms",
+                    ConfigurationManagerImpl::GetDefaultInstance().GetFactoryResetDuration().count());
+#endif // CONFIG_CHIP_FACTORY_RESET_TIME_MEASUREMENT
 
     PlatformMgr().Shutdown();
 }

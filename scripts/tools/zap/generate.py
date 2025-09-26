c34b9e57f0cea9e010c140a02b6e63954ef91bbe
Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>
2025-09-26 10:30:01 +0200

    [nrf toup] [scripts] Fix running generate for Windows
    If the host system is Windows, we need to add `python3` to the
    subprocess command to avoid issues like:
    `OSError: [WinError 193] %1 is not a valid Win32 application`
    
    Also, inform Windows users what they can do if paths to the
    files are too long, and how to resolve issues like:
    `FileNotFoundError: [WinError 206] The filename or extension is
    too long`
    
    Signed-off-by: Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>

--- scripts/tools/zap/generate.py
+++ scripts/tools/zap/generate.py
@@ -296,7 +296,14 @@ def runClangPrettifier(templates_file, output_dir):
             clang_format = getClangFormatBinary()
             args = [clang_format, '-i']
             args.extend(clangOutputs)
-            subprocess.check_call(args)
+            try:
+                subprocess.check_call(args)
+            except FileNotFoundError as e:
+                print(e)
+                print("On Windows, the filename or extension may be too long to process it here.")
+                print("You can try to fix it by changing the LongPathsEnabled inside the \HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\FileSystem registry key from 0 to 1.")
+                return
+
             print('Formatted %d files using %s (%s)' %
                   (len(clangOutputs), clang_format, subprocess.check_output([clang_format, '--version'])))
             for outputName in clangOutputs:

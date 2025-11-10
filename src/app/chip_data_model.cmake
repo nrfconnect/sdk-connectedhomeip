d6972478d84a3843ca95246b28cdcc0401120d52
Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>
2025-11-10 09:30:27 +0100

    Revert "[nrf noup] Bypassed code generation at build time"
    This reverts commit 9f29a2b22af810cffcc8c44ca2f341114349eac7.

--- src/app/chip_data_model.cmake
+++ src/app/chip_data_model.cmake
@@ -16,10 +16,6 @@
 
 set(CHIP_APP_BASE_DIR ${CMAKE_CURRENT_LIST_DIR})
 
-if (NOT CHIP_ROOT)
-    get_filename_component(CHIP_ROOT ${CHIP_APP_BASE_DIR}/../.. REALPATH)
-endif()
-
 if (NOT CHIP_APP_ZAP_DIR)
     get_filename_component(CHIP_APP_ZAP_DIR ${CHIP_ROOT}/zzz_generated/app-common REALPATH)
 endif()
@@ -66,20 +62,19 @@ endfunction()
 #
 # Configure ${APP_TARGET} based on the selected data model configuration.
 # Available options are:
-#   SCOPE           CMake scope keyword that defines the scope of included sources.
-#                   The default is PRIVATE scope.
-#   BYPASS_IDL      Bypass code generation from .matter IDL file.
-#   ZAP_FILE        Path to the ZAP file, used to determine the list of clusters
-#                   supported by the application.
-#   IDL             .matter IDL file to use for codegen. Inferred from ZAP_FILE
-#                   if not provided
-#   EXTERNAL_CLUSTERS Clusters with external implementations. The default implementations
-#                   will not be used nor required for these clusters.
-#                   Format: MY_CUSTOM_CLUSTER'.
+# SCOPE             CMake scope keyword that defines the scope of included sources.
+# The default is PRIVATE scope.
+# ZAP_FILE          Path to the ZAP file, used to determine the list of clusters
+# supported by the application.
+# IDL               .matter IDL file to use for codegen. Inferred from ZAP_FILE
+# if not provided
+# EXTERNAL_CLUSTERS Clusters with external implementations. The default implementations
+# will not be used nor required for these clusters.
+# Format: MY_CUSTOM_CLUSTER'.
 #
 function(chip_configure_data_model APP_TARGET)
     set(SCOPE PRIVATE)
-    cmake_parse_arguments(ARG "BYPASS_IDL" "SCOPE;ZAP_FILE;GEN_DIR;IDL" "EXTERNAL_CLUSTERS" ${ARGN})
+    cmake_parse_arguments(ARG "" "SCOPE;ZAP_FILE;IDL" "EXTERNAL_CLUSTERS" ${ARGN})
 
     if(ARG_SCOPE)
         set(SCOPE ${ARG_SCOPE})
@@ -110,7 +105,7 @@ function(chip_configure_data_model APP_TARGET)
         endif()
     endif()
 
-    if (ARG_IDL AND NOT ARG_BYPASS_IDL)
+    if(ARG_IDL)
         chip_codegen(${APP_TARGET}-codegen
             INPUT "${ARG_IDL}"
             GENERATOR "cpp-app"
@@ -125,50 +120,24 @@ function(chip_configure_data_model APP_TARGET)
 
         target_include_directories(${APP_TARGET} ${SCOPE} "${APP_GEN_DIR}")
         add_dependencies(${APP_TARGET} ${APP_TARGET}-codegen)
-
-        chip_zapgen(${APP_TARGET}-zapgen
-            INPUT "${ARG_ZAP_FILE}"
-            GENERATOR "app-templates"
-            OUTPUTS
-            "zap-generated/access.h"
-            "zap-generated/CHIPClientCallbacks.h"
-            "zap-generated/endpoint_config.h"
-            "zap-generated/gen_config.h"
-            "zap-generated/IMClusterCommandHandler.cpp"
-            OUTPUT_PATH APP_TEMPLATES_GEN_DIR
-            OUTPUT_FILES APP_TEMPLATES_GEN_FILES
-        )
-        target_include_directories(${APP_TARGET} ${SCOPE} "${APP_TEMPLATES_GEN_DIR}")
-        add_dependencies(${APP_TARGET} ${APP_TARGET}-zapgen)
     else()
-        target_compile_definitions(${APP_TARGET} PRIVATE CHIP_BYPASS_IDL)
-        target_include_directories(${APP_TARGET} ${SCOPE} ${ARG_GEN_DIR})
-        set(APP_GEN_FILES
-            ${ARG_GEN_DIR}/callback-stub.cpp
-            ${ARG_GEN_DIR}/IMClusterCommandHandler.cpp
-        )
+        set(APP_GEN_FILES)
     endif()
 
-    # These are:
-    #   //src/app/icd/server:notfier
-    #   //src/app/icd/server:monitoring-table
-    #   //src/app/icd/server:configuration-data
-    #
-    # TODO: ideally we would avoid duplication and would link gn-built items. In this case
-    #       it may be slightly harder because these are source_sets rather than libraries.
-    target_sources(${APP_TARGET} ${SCOPE}
-        ${CHIP_APP_BASE_DIR}/icd/server/ICDMonitoringTable.cpp
-        ${CHIP_APP_BASE_DIR}/icd/server/ICDNotifier.cpp
-        ${CHIP_APP_BASE_DIR}/icd/server/ICDConfigurationData.cpp
+    chip_zapgen(${APP_TARGET}-zapgen
+        INPUT "${ARG_ZAP_FILE}"
+        GENERATOR "app-templates"
+        OUTPUTS
+        "zap-generated/access.h"
+        "zap-generated/endpoint_config.h"
+        "zap-generated/gen_config.h"
+        "zap-generated/IMClusterCommandHandler.cpp"
+        OUTPUT_PATH APP_TEMPLATES_GEN_DIR
+        OUTPUT_FILES APP_TEMPLATES_GEN_FILES
     )
-
-    # This is:
-    #    //src/app/common:cluster-objects
-    #
-    # TODO: ideally we would avoid duplication and would link gn-built items
-    target_sources(${APP_TARGET} ${SCOPE}
-        ${CHIP_APP_BASE_DIR}/../../zzz_generated/app-common/app-common/zap-generated/cluster-objects.cpp
-    )
+    target_include_directories(${APP_TARGET} ${SCOPE} "${APP_TEMPLATES_GEN_DIR}")
+    target_include_directories(${APP_TARGET} ${SCOPE} "${CHIP_APP_BASE_DIR}/zzz_generated")
+    add_dependencies(${APP_TARGET} ${APP_TARGET}-zapgen)
 
     target_sources(${APP_TARGET} ${SCOPE}
         ${CHIP_APP_ZAP_DIR}/app-common/zap-generated/attributes/Accessors.cpp

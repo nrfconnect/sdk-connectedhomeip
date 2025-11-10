15e32192cde74fb85b68e984c85eb72e8ed3a5ff
Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>
2025-11-10 09:30:27 +0100

    Revert "[nrf noup] Shedule factory reset in Server using Matter Shell command"
    This reverts commit b6126541def0a6c8acebb7d5720134fc0d8e35ff.

--- src/lib/shell/commands/Device.cpp
+++ src/lib/shell/commands/Device.cpp
@@ -15,7 +15,6 @@
  *    limitations under the License.
  */
 
-#include <app/server/Server.h>
 #include <lib/shell/Commands.h>
 #include <lib/shell/Engine.h>
 #include <lib/shell/SubShellCommand.h>
@@ -28,11 +27,7 @@ namespace Shell {
 static CHIP_ERROR FactoryResetHandler(int argc, char ** argv)
 {
     streamer_printf(streamer_get(), "Performing factory reset ... \r\n");
-#if CHIP_CONFIG_TEST
     DeviceLayer::ConfigurationMgr().InitiateFactoryReset();
-#else
-    chip::Server::GetInstance().ScheduleFactoryReset();
-#endif // CHIP_TEST
     return CHIP_NO_ERROR;
 }
 

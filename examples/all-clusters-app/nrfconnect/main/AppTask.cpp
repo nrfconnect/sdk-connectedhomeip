55b3a709e062fbc955e10467d507370443a39ff1
Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>
2025-11-10 09:30:27 +0100

    Revert "[nrf toup][nrfconnect] Align OpenThread Kconfigs with Zephyr changes"
    This reverts commit 08ba5211b355408e81069febc117301a35646755.

--- examples/all-clusters-app/nrfconnect/main/AppTask.cpp
+++ examples/all-clusters-app/nrfconnect/main/AppTask.cpp
@@ -164,7 +164,7 @@ CHIP_ERROR AppTask::Init()
         return err;
     }
 
-#if defined(CONFIG_OPENTHREAD)
+#if defined(CONFIG_NET_L2_OPENTHREAD)
     err = ThreadStackMgr().InitThreadStack();
     if (err != CHIP_NO_ERROR)
     {
@@ -188,7 +188,7 @@ CHIP_ERROR AppTask::Init()
     sWiFiCommissioningInstance.Init();
 #else
     return CHIP_ERROR_INTERNAL;
-#endif // CONFIG_OPENTHREAD
+#endif // CONFIG_NET_L2_OPENTHREAD
 
     // Initialize LEDs
     LEDWidget::InitGpio();
@@ -542,7 +542,7 @@ void AppTask::ChipEventHandler(const ChipDeviceEvent * event, intptr_t /* arg */
         sHaveBLEConnections = ConnectivityMgr().NumBLEConnections() != 0;
         UpdateStatusLED();
         break;
-#if defined(CONFIG_OPENTHREAD)
+#if defined(CONFIG_NET_L2_OPENTHREAD)
     case DeviceEventType::kDnssdInitialized:
 #if CONFIG_CHIP_OTA_REQUESTOR
         InitBasicOTARequestor();
